f81adfb426cc02cb4e7e4bac498a1c9e9a1435a0
DmC-sleVen <56522129+DmC-sleVen@users.noreply.github.com>
2020-05-30 12:56:29 +0200

    Initial py code commit
    Python code with basic web-crawler functionality..

---
+++ py_web_crawler.py
@@ -0,0 +1,47 @@
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import time
+
+class MietenFetcher():
+    def fetch(self):
+        url = "https://www.immobilienscout24.de/Suche/de/baden-wuerttemberg/freiburg-im-breisgau/wohnung-kaufen"
+        
+        while url != "":
+            print(url)
+            time.sleep(1)
+            r = requests.get(url)
+            dok = BeautifulSoup(r.text, "html.parser")    
+
+            
+            for i in dok.select(".font-nowrap"):
+                splitted = i.text.split()
+                if splitted[1]=="€":
+                    Mieten = splitted[0]
+                    print(Mieten, "€")
+                    
+                
+
+            next_page = dok.select_one(".icon-arrow-forward")
+            if next_page:
+                next_page = next_page.attrs["href"]
+                baseurl = "https://www.immobilienscout24.de"
+                nexturl = urljoin(baseurl, next_page)
+                url = nexturl
+            else:
+                url = ""
+            
+
+
+    def jahresnettokaltmiete(self):
+        pass
+
+
+        
+
+fetcher=MietenFetcher()
+fetcher.fetch()
+for element in fetcher.fetch():
+    print(element)
+
+    
